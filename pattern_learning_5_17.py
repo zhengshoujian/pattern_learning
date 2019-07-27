56b7a0b3ed57904d8b53660e1f7c0a4ba8143da2
zhengshoujian <2239779352@qq.com>
2019-07-27 14:09:27 +0800

    rm useless code

--- pattern_learning_5_17.py
+++ pattern_learning_5_17.py
@@ -26,7 +26,6 @@ def load_data( filename ):
         return data[b'data'], data[b'labels'] # 并且 在 key 前需要加上 b
 
 
-
 train_filename_normal = [os.path.join(CIFAR_DIR, 'data_batch_%d' % i) for i in range(1, 5)]
 train_filename_pattern = [os.path.join(CIFAR_DIR, 'data_batch_%d' % i) for i in range(5, 6)]
 test_filename = [os.path.join(CIFAR_DIR, 'test_batch')]
@@ -37,8 +36,8 @@ test_data = CifarData( test_filename, False )
 
 
 batch_size = 4
-x = tf.placeholder( tf.float32, [None, 3072] )
-y = tf.placeholder( tf.int64, [None] ) 
+x = tf.placeholder(tf.float32, [None, 3072])
+y = tf.placeholder(tf.int64, [None])
 
 x_image = tf.reshape( x, [-1, 3, 32, 32] )
 x_image = tf.transpose( x_image, perm= [0, 2, 3, 1] )
@@ -51,20 +50,15 @@ loss = tf.losses.sparse_softmax_cross_entropy( labels = y, logits = y_ )
 # 该api,做了三件事儿 1. y_ -> softmax 2. y -> one_hot 3. loss = ylogy
 
 # 预测值 获得的是 每一行上 最大值的 索引.注意:tf.argmax()的用法,其实和 np.argmax() 一样的
-predict = tf.argmax( y_, 1 )
+predict = tf.argmax(y_, 1)
 # 将布尔值转化为int类型,也就是 0 或者 1, 然后再和真实值进行比较. tf.equal() 返回值是布尔类型
-correct_prediction = tf.equal( predict, y )
+correct_prediction = tf.equal(predict, y)
 # 比如说第一行最大值索引是6,说明是第六个分类.而y正好也是6,说明预测正确
-
-
 
 accuracy = tf.reduce_mean( tf.cast(correct_prediction, tf.float64) )
 
-with tf.name_scope( 'train_op_small' ): # tf.name_scope() 这个方法的作用不太明白(有点迷糊!)
-    train_op_small = tf.train.AdamOptimizer(1e-4).minimize( loss ) # 将 损失函数 降到 最低
-    
-with tf.name_scope( 'train_op_big' ): # tf.name_scope() 这个方法的作用不太明白(有点迷糊!)
-    train_op_big = tf.train.AdamOptimizer(1e-3).minimize( loss ) # 将 损失函数 降到 最低
+with tf.name_scope( 'train_op' ):
+    train_op = tf.train.AdamOptimizer(1e-3).minimize( loss ) # 将 损失函数 降到 最低
 
 # 初始化变量
 init = tf.global_variables_initializer()
@@ -76,34 +70,26 @@ test_steps = 300
 train_step_pattern = 10000
 
 time_now = time.strftime('%Y.%m.%d',time.localtime(time.time()))
-loss_sum = []
 loss_before = 5.0
 loss_sum.append(loss_before)
-count_iter = 0
-count_no_iter = 0
-count_1 = 0
-count_2 = 0
 
 batch_count_max = 7
 batch_count = 0
 
-length_of_loss_aver = 1
-
 test_acc_val = []
 
+count_1 = 0
 list_count_1 =[]
 loss_val_1 = []
 
+count_2 = 0
 list_count_2 =[]
 loss_val_2 =[]
-
-pattern1 = "loss_before>loss_val"
-pattern2 = "loss_before<loss_val"
 
 #f = open("output",'a+')
 update_1 = True
 updata_2 = True
-f = open('result.txt','a+')
+f = open('result.txt', 'a+')
 saver = tf.train.Saver()
 with tf.Session() as sess:
 	sess.run( init ) # 注意: 这一步必须要有!!
@@ -114,7 +100,7 @@ with tf.Session() as sess:
 		
 		loss_val, acc_val = sess.run( [loss, accuracy], feed_dict={x:batch_data, y:batch_labels} )
 		loss_before = loss_val
-		sess.run([train_op_big], feed_dict={x:batch_data, y:batch_labels} )
+		sess.run([train_op], feed_dict={x:batch_data, y:batch_labels} )
 		
 		if(count_1%200==0):
 			list_count_1.append(count_1)
@@ -129,7 +115,7 @@ with tf.Session() as sess:
 			else:
 				#batch_count not too much
 				batch_count+=1
-				sess.run([train_op_big], feed_dict={x:batch_data, y:batch_labels} )
+				sess.run([train_op], feed_dict={x:batch_data, y:batch_labels} )
 				
 				if(count_1%20==0):
 					list_count_1.append(count_1)
@@ -144,7 +130,7 @@ with tf.Session() as sess:
 		if ( i+1 ) % 1 == 0:
 			print('\n[Train] Step: %d, loss: %4.8f, acc: %4.8f' % ( i+1, loss_val, acc_val ))
 		# 每 5000 次 进行一次 测试
-	test_acc=0;
+	test_acc = 0;
 	test_data = CifarData( test_filename, False )
 	all_test_acc_val = []
 	for j in range( test_steps ):
@@ -160,19 +146,8 @@ with tf.Session() as sess:
 	print('\n[Test ] Step: %d, acc: %4.5f' % ( (i+1), test_acc ))
 	print('----------------------------------------------------')
 
-
-	#save_path = saver.save(sess, model_save_path)
-	#print("Model saved in file: %s" % save_path)
-	#line = time_now + ' ' + str(count_iter) + ' ' + str(count_no_iter) + ' ' + str(test_acc) + ' ' + str(loss_no_cout[-1]) + '\n'
-	#f.write(line)
-
-
 	save_path = saver.save(sess, model_save_path)
 	print("Model saved in file: %s" % save_path)
-	#line = time_now + ' ' + str(count_iter) + ' ' + str(count_no_iter) + ' ' + str(test_acc) + ' ' + '\n'
-	#f.write(line)
-	#f.close()
-
 
 with tf.Session() as sess:
     sess.run( init ) # 注意: 这一步必须要有!!
@@ -184,7 +159,7 @@ with tf.Session() as sess:
 
         loss_val, acc_val = sess.run( [loss, accuracy], feed_dict={x:batch_data, y:batch_labels} )
 
-        sess.run([train_op_big], feed_dict={x:batch_data, y:batch_labels} )
+        sess.run([train_op], feed_dict={x:batch_data, y:batch_labels} )
             
         count_2+=1
         
@@ -210,13 +185,9 @@ with tf.Session() as sess:
     line ="normal learning:"+' '*23 + str(time.time()) +' batch_size = '+str(batch_size)+" "*20 +' normal_learning_test acc = ' + str(test_acc)+" batch_size = "+str(batch_size) + '\n'
     print(line)
     f.write(line)
-            
-            
+
     save_path = saver.save(sess, model_save_path)
     print("Model saved in file: %s" % save_path)
-    #line = time_now + ' ' + str(count_iter) + ' ' + str(count_no_iter) + ' ' + str(test_acc) + ' ' + '\n'
-    #f.write(line)
-    #f.close()
     f.close()
 
 
